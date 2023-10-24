cbf8cb4a2d7f79506b7a9442396091241169b2f0
MikkoAyaka <3401286177@qq.com>
2023-10-24 23:04:24 +0800

    整理 CacheConfig 注解

--- SharineBackend/src/main/java/org/tcpx/sharine/backend/service/UserService.java
+++ SharineBackend/src/main/java/org/tcpx/sharine/backend/service/UserService.java
@@ -1,6 +1,7 @@
 package org.tcpx.sharine.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -10,29 +11,29 @@ import org.tcpx.sharine.backend.repository.UserRepository;
 
 import java.util.Optional;
 
+@CacheConfig(cacheNames = "users")
 @Service
 public class UserService extends BaseService<User,Integer>{
 
-    private static final String cacheNames = "users";
     @Autowired
     public UserService(UserRepository repository) {
         super.repository = repository;
     }
 
     @Override
-    @CachePut(cacheNames = cacheNames, key = "#entity.id")
+    @CachePut(key = "#entity.id")
     public void insert(User entity) {
         super.insert(entity);
     }
 
     @Override
-    @Cacheable(cacheNames = cacheNames,key = "#id")
+    @Cacheable(key = "#id")
     public Optional<User> find(Integer id) {
         return super.find(id);
     }
 
     @Override
-    @CacheEvict(cacheNames = cacheNames,key = "#id")
+    @CacheEvict(key = "#id")
     public void delete(Integer id) {
         super.delete(id);
     }
