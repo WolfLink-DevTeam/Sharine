c4ed64cc10db7793e198c147f95d8c216e441928
MikkoAyaka <3401286177@qq.com>
2023-10-24 14:54:41 +0800

    首次提交，初始化后端SpringBoot仓库

---
+++ Sharine-Backend/src/main/java/org/tcpx/sharine/backend/SharineBackendApplication.java
@@ -0,0 +1,13 @@
+package org.tcpx.sharine.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharineBackendApplication.class, args);
+	}
+
+}
---
+++ Sharine-Backend/src/test/java/org/tcpx/sharine/backend/SharineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tcpx.sharine.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
