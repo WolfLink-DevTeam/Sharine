ca1a9b9bcd7ef40213aad065c5c25764de5909da
lunave <lunave@proton.me>
2023-10-25 16:35:03 +0800

    CacheConfig

---
+++ server/src/main/java/org/tcpx/sharine/config/CacheConfig.java
@@ -0,0 +1,48 @@
+package org.tcpx.sharine.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+    @Bean
+    public CacheManager cacheManager(RedisTemplate<String, Object> template) {
+        RedisCacheConfiguration defaultCacheCfg = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofMinutes(30))
+                .serializeKeysWith(SerializationPair.fromSerializer(template.getStringSerializer()))
+                .serializeValuesWith(SerializationPair.fromSerializer(template.getValueSerializer()));
+
+        return RedisCacheManager.builder(template.getRequiredConnectionFactory())
+                .cacheDefaults(defaultCacheCfg)
+                .transactionAware()
+                .build();
+    }
+}
