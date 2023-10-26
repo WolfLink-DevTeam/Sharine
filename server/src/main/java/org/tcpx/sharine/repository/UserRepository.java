3d173803031654705390fc80865aefa40a2ca640
MikkoAyaka <3401286177@qq.com>
2023-10-26 22:17:13 +0800

    删除多余的@Repository注解

--- server/src/main/java/org/tcpx/sharine/repository/UserRepository.java
+++ server/src/main/java/org/tcpx/sharine/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package org.tcpx.sharine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.tcpx.sharine.entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
