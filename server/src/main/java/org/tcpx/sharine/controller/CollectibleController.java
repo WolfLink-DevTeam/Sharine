669bae6846494b1e7cf44514ea11d67ac45efeca
MikkoAyaka <3401286177@qq.com>
2023-10-29 15:14:19 +0800

    补充 Collectible 的 Service,Repository,Controller 类

---
+++ server/src/main/java/org/tcpx/sharine/controller/CollectibleController.java
@@ -0,0 +1,7 @@
+package org.tcpx.sharine.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/collectible")
+public class CollectibleController extends BaseController {
+}
---
+++ server/src/main/java/org/tcpx/sharine/service/CollectibleService.java
@@ -0,0 +1,7 @@
+package org.tcpx.sharine.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CollectibleService {
+}
