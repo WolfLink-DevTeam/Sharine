e32c4b2ba613188213a5cd7ecb7d4dd682b037bb
MikkoAyaka <3401286177@qq.com>
2023-11-05 13:22:02 +0800

    在用户关注视频时会同时关注视频作者 取消关注时也会取消关注视频作者

--- server/src/main/java/org/tcpx/sharine/repository/UserRelationRepository.java
+++ server/src/main/java/org/tcpx/sharine/repository/UserRelationRepository.java
@@ -1,8 +1,10 @@
 package org.tcpx.sharine.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.tcpx.sharine.constants.DatabaseConst;
 import org.tcpx.sharine.entity.UserRelation;
@@ -13,6 +15,9 @@ import java.util.List;
 @CacheConfig(cacheNames = DatabaseConst.USER_RELATION)
 public interface UserRelationRepository extends JpaRepository<UserRelation, Long> {
 
+    @Modifying
+    @Transactional
+    void deleteByUserId1AndUserId2(Long userId1,Long userId2);
     @Query("SELECT COUNT(f) FROM UserRelation f " +
             "WHERE (f.userId1 = :userId AND f.status = 1) " +
             "OR (f.userId2 = :userId AND f.status = 2)")
