b5ba700d2b82d58484850ccecba5330a7a1c738f
MikkoAyaka <3401286177@qq.com>
2023-11-06 18:38:38 +0800

    配置环境变量

--- web/src/utilities/HttpUtility.ts
+++ web/src/utilities/HttpUtility.ts
@@ -1,33 +1,35 @@
 import axios, {AxiosPromise} from "axios";
 import {ResponsePack} from "@/models/ResponsePack";
 import {userService} from "@/services/UserService";
+
 export const httpClient = axios.create({
-    baseURL: 'http://127.0.0.1:8080',
-    timeout: 1000
+    baseURL: import.meta.env.VITE_SHARINE_API_BASE_URL,
+    timeout: Number(import.meta.env.VITE_SHARINE_API_TIMEOUT)
 });
+
 httpClient.interceptors.request.use(
-    (config) => {
+    (config: any) => {
         // 在发送请求之前做些什么
         config.headers['satoken'] = userService.getToken();
         return config;
     }
 );
 httpClient.interceptors.response.use(
-    (response) => {
+    (response: any) => {
         if(response?.data?.msg?.startsWith("程序内部错误: token 无效：")) {
             alert("Token 已过期，请重新登录！")
         }
         return response;
     },
-    (error) => {
+    (error: any) => {
         return Promise.reject(error);
     }
 );
 
 export function pack(axiosPromise: AxiosPromise<any>): Promise<ResponsePack> {
-    return axiosPromise.then(function (response) {
+    return axiosPromise.then(function (response : any) {
         return ResponsePack.of(response)
-    }).catch(function (error) {
+    }).catch(function (error: any) {
         console.log(error);
         return ResponsePack.ofEmpty()
     });
